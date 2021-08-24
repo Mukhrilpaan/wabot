02c4ec4c175a33645ffb96e8721e4c0539e8827a
Mukhrilpaan <89409299+Mukhrilpaan@users.noreply.github.com>
2021-08-24 03:12:14 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,36 @@
+global.owner = ['61032971585'] // Put your number here
+global.mods = [] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': '4c7ec8d2aba357e8',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
+}
+
+// Sticker WM
+global.packname = 'Jangan curi yee😙'
+global.author = 'Paanlah siapa lagi🥺😙👀✨'
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
